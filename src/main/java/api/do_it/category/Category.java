d3fa52d31420d084c19a2bafbe7896ffa56d5b44
igor-de-andrade <cadastros.igordeandrade@gmail.com>
2025-01-02 20:49:36 -0300

    Fixed an error on the findById end-point of categories

--- src/main/java/api/do_it/category/Category.java
+++ src/main/java/api/do_it/category/Category.java
@@ -1,11 +1,13 @@
 package api.do_it.category;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
 @Table(name="categories")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Category {
 	
 	@Id
